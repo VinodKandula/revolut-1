d741e15a7922dae4c8cd68a91e3a9a2c0ef666ac
Ilya Ismagilov <ismagilov@gmail.com>
2017-10-07 14:01:25 +0700

    Setup SQL model

--- src/main/java/TransfersServer.java
+++ src/main/java/TransfersServer.java
@@ -1,22 +1,22 @@
-import db.tables.records.NameRecord;
+import db.tables.records.AccountRecord;
 import org.h2.jdbcx.JdbcConnectionPool;
 import org.jooq.DSLContext;
 import org.jooq.Result;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 
-import static db.tables.Name.NAME;
+import static db.tables.Account.ACCOUNT;
 import static spark.Spark.get;
 
 public class TransfersServer {
+    private static final String DB_URL = "jdbc:h2:mem:transfers;" +
+            "INIT=RUNSCRIPT FROM 'classpath:/h2/schema.sql'\\;RUNSCRIPT FROM 'classpath:/h2/data.sql';";
+
     public static void main(String[] args) {
-        JdbcConnectionPool pool = JdbcConnectionPool.create(
-                "jdbc:h2:mem:transfers;INIT=" +
-                        "RUNSCRIPT FROM 'classpath:/h2/schema.sql'\\;RUNSCRIPT FROM 'classpath:/h2/data.sql';",
-                "sa", "");
+        JdbcConnectionPool pool = JdbcConnectionPool.create(DB_URL, "sa", "");
         DSLContext ctx = DSL.using(pool, SQLDialect.H2);
 
-        Result<NameRecord> rs = ctx.selectFrom(NAME).fetch();
+        Result<AccountRecord> rs = ctx.selectFrom(ACCOUNT).fetch();
 
         System.out.println(rs.formatJSON());
 
