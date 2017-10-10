bbb8cb1946067a0ba5294db3b3b1bc9dce7c1ca4
Ilya Ismagilov <ismagilov@gmail.com>
2017-10-10 17:48:36 +0700

    Delete fixed TODO

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -2,7 +2,6 @@ package model;
 
 import java.math.BigDecimal;
 
-//TODO Create new class for accounts creation: AccountCreation
 public class Account {
     public long id;
     public String number;
